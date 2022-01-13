17b5070333476a6ab80be0f9e22f820c5e89329d
Andrea Stella <andreaste00@gmail.com>
2022-01-13 12:19:34 +0100

    Add config file for bitracker configurations and parsing functions.

--- client/cli/config_parser.py
+++ client/cli/config_parser.py
@@ -1,16 +1,19 @@
 import configparser
 
+from client.cli.constants import BITRACKER_CONFIG_PATH
+
+# read_config reads the bitracker config file and returns it.
 def read_config():  
     config = configparser.ConfigParser()
-    config.read('./bitracker.cfg')
+    config.read(BITRACKER_CONFIG_PATH)
     return config
 
-# TODO
+# write_config writes the bitracker config file.
+# key should be a string, representing the first-level element in the [].
+# val should be a string or a dictionary, representing the elements under the []. 
 def write_config(key, val):
     config = configparser.ConfigParser()
     config[key] = {}
-    config[key]['KEY'] = val
-    # TODO change to another config file position
-    # in ~/.bitracker/bitracker.cfg
-    with open('./bitracker.cfg', 'w') as conf:
+    config[key] = val
+    with open(BITRACKER_CONFIG_PATH, 'w') as conf:
         config.write(conf)
